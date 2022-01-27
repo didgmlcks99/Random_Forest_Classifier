134a646a90e14fe997392598d9b268ea69e38135
didgmlcks99 <heechy06@naver.com>
2022-01-27 23:16:50 +0900

    finish 2nd retest on low opt

--- src/trainer.py
+++ src/trainer.py
@@ -90,7 +90,7 @@ if run_case == True:
     name = '../analysis/main/main.csv'
     # recorder.init_test_analysis(name)
     
-    while low_freq <= 6000:
+    while low_freq <= 5000:
 
         modelizer.finalize_model(tmp_model, high_freq, low_freq, tmp_neg_count_dict, tmp_non_count_dict, run_case, default_sort_order)
         recorder.direct_test(name, str(high_freq)+'/'+str(low_freq))
